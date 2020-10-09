5eddfb4268a0e6be765f53622732dddc7117782a
Rachel <racheljay42@gmail.com>
2020-10-09 10:33:09 -0400

    zero disappears, broken AC

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 const main = document.getElementById('main');
-let num = ['0'];
+
 
 class Calculator {
     constructor() {
-        this.operators = ["+", "-", "*", "/", "=", "AC"];
+        this.operators = ["+", "-", "*", "/"];
+        // this.clear = "AC"
         this.display = null;
+        this.num = [0];
     }
 
-    
+
     view() {
         //make the calc display 
         const container = document.createElement('div');
@@ -17,7 +19,8 @@ class Calculator {
         const display = document.createElement('div');
         display.setAttribute('class', 'col-12 border');
         //how do I get this variable to come visit me in Button.btnAction()?
-        display.innerHTML = num;
+        // let num = ['0'];
+        display.innerHTML = this.num;
         this.display = display;
 
         main.appendChild(container).appendChild(displayRow).appendChild(display);
@@ -26,19 +29,27 @@ class Calculator {
         numBtnBox.setAttribute('class', 'row');
         container.appendChild(numBtnBox);
 
-        for(let i = 0; i <= 9; i++) {
-            let numBtn = new Number(i, display); //<= HELLO this is where display gets passed to Child
+        //make the number buttons
+        for (let i = 0; i <= 9; i++) {
+            let numBtn = new Number(i, display, this.num); //<= HELLO this is where display gets passed to Child
             numBtn.btnAction();
             numBtnBox.appendChild(numBtn.col);
         }
 
-        for(let i = 0; i < this.operators.length; i++) {
+        //make the operator buttons
+        for (let i = 0; i < this.operators.length; i++) {
             let oppBtn = new Operator(this.operators[i]);
             oppBtn.makeBtn();
             numBtnBox.appendChild(oppBtn.col);
         }
 
+        //make a clear button
+        let clearBtn = new Clear("AC", display, this.num);
+        clearBtn.btnAction();
+        numBtnBox.appendChild(clearBtn.col);
+
     }
+
 
     calculate() {
         let calcArr = [];
@@ -49,12 +60,7 @@ class Button {
     constructor(id) {
         this.id = id;
         this.col = null;
-        // this.childFunction = this.childFunction.bind(this);
     }
-
-    // childFunction() {
-    //     console.log(this.data);
-    // }
 
     makeBtn() {
         let btn = document.createElement('div');
@@ -64,38 +70,42 @@ class Button {
         this.col = btn;
     }
 
-    
 
-   
 }
 
-class Number extends Button { 
-    constructor(id, displayFromParent) {
-    super(id);
-    this.display = displayFromParent
-    this.numBtnAction = this.numBtnAction.bind(this);
+class Number extends Button {
+    constructor(id, displayFromParent, numFromParent) {
+        super(id);
+        this.display = displayFromParent
+        this.numFromParent = numFromParent;
+        this.numBtnAction = this.numBtnAction.bind(this);
     }
-        btnAction() {
-            this.makeBtn();
-            console.log(`Number: ${this.id}`);
-            this.col.addEventListener('click', this.numBtnAction);
+    btnAction() {
+        this.makeBtn();
+        //console.log(`Number: ${this.id}`);
+        this.col.addEventListener('click', this.numBtnAction);
 
-        }
-
-        numBtnAction() {
-            num.push(this.id);
-            let displayNum = num.join(' ');
-            this.display.innerHTML = displayNum;
-            console.log(this.id);
-        }
     }
+
+    numBtnAction() {
+        this.numFromParent.push(this.id);
+        if (this.numFromParent[0] === 0) {
+            this.numFromParent.shift();
+            // return this.numBtnAction;
+        }
+        console.log(this.numFromParent);
+        let displayNum = this.numFromParent.join(' ');
+        this.display.innerHTML = displayNum;
+        console.log(this.id);
+    }
+}
 
 //if I have my makebtn method, how do I sub in extra lines?
 
 
 class Operator extends Button {
-    constructor(id){
-    super(id)
+    constructor(id) {
+        super(id)
     }
 
     btnAction() {
@@ -110,6 +120,30 @@ class Operator extends Button {
         console.log(this.id);
     }
 }
+
+class Clear extends Button {
+    constructor(id, displayFromParent, numFromParent) {
+        super(id);
+        this.display = displayFromParent;
+        this.numFromParent = numFromParent;
+        this.clearBtnAction = this.clearBtnAction.bind(this);
+    }
+    btnAction() {
+        this.makeBtn();
+        //console.log(`Number: ${this.id}`);
+        this.col.addEventListener('click', this.clearBtnAction)
+    }
+
+    clearBtnAction() {
+        if (this.numFromParent !== 0) {
+            this.numFromParent = [0]
+            this.display.innerHTML = [0]
+            // return this.numBtnAction;
+        }
+       console.log(this.id) 
+    }
+}
+
 
 const calc = new Calculator();
 calc.view();
